54d4b80ff462977bba0ce6ae933189a23dbf6354
sashaij <o.zagorodniuk@outlook.com>
2023-11-09 09:50:18 +0200

    Select form dom elements, create book constructor func

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 'use strict'
 
+const bAuthor = document.getElementById('author');
+const bTitle = document.getElementById('title');
+const bYear = document.getElementById('year');
+const bGenre = document.getElementById('genre');
+const bCountry = document.getElementById('country');
+const bOriginalLang = document.getElementById('original-lang');
+const bFormat = document.getElementById('format');
+const numberOfPages = document.getElementById('num-pages');
+const readStatus = document.getElementById('status');
+const buttonSubmit = document.getElementById('submit-button');
+const togFormat = document.getElementsByName('format');
+const togStatus = document.getElementsByName('status');
+
+function Book (title, author, year, 
+    genre, country, originalLang, 
+    pagesNum, bookFormat, status) {
+this.title = title;
+this.author = author;
+this.year = year;
+this.genre = genre;
+this.country = country;
+this.originalLang = originalLang;
+this.bookFormat = bookFormat;
+this.pagesNum = pagesNum;
+this.status = status;
+}
+
+
+
 /* const item = {
     name: 'JavaScript',
     price: 59.99,
