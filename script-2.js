5974e7631c513210a763bdb36cd2fb727fd4cc55
sashaij <o.zagorodniuk@outlook.com>
2023-11-18 11:32:17 +0200

    Add main copies of main files

---
+++ script-2.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const bAuthor = document.getElementById('author');
+const bTitle = document.getElementById('title');
+const bYear = document.getElementById('year');
+const bGenre = document.getElementById('genre');
+const bCountry = document.getElementById('country');
+const bOriginalLang = document.getElementById('original-lang');
+const bFormat = document.getElementById('format');
+const numberOfPages = document.getElementById('num-pages');
+const readStatus = document.getElementById('status');
+const buttonSubmit = document.getElementById('submit-button');
+const togFormat = document.getElementsByName('format');
+const togStatus = document.getElementsByName('status');
+const cardsContainer = document.querySelector('.cards-container');
